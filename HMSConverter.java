908224a37d7da74ad7ef572541cd3e2a7ffe5cd6
Billy Sturman <billysturman@u.boisestate.edu>
2025-02-02 20:27:37 -0700

    first commit

---
+++ HMSConverter.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+ 
+public class HMSConverter 
+{
+   	public static void main(String[] args)
+   	{
+		Scanner scnr = new Scanner(System.in);
+		int numHours;
+        int numMinutes;
+        int numSeconds;
+		int totalSeconds;
+
+		System.out.println("Enter the number of hours:");
+		numHours = scnr.nextInt();
+
+        System.out.println("Enter the number of minutes:");
+		numMinutes = scnr.nextInt();
+
+        System.out.println("Enter the number of seconds:");
+		numSeconds = scnr.nextInt();
+
+        totalSeconds = (numHours * 3600) + (numMinutes * 60) + numSeconds;
+
+		System.out.println("Total seconds: " + totalSeconds);
+
+   	}
+}
